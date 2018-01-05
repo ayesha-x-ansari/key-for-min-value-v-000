d4a3f21040f09c755e8181f1fca6b9004d967658
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-05 15:10:23 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,9 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(ikea)
-  min_value =  0
+  min_value =  9999999
   min_key   =  nil
-  ikea.each do  |key, value| if value > min_value
+  ikea.each do  |key, value| if value <  min_value
                     min_value = value
                     puts min_value
                     puts min_key
