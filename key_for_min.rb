122d2817c790389cefdac0eafac46a56584f9d22
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-05 15:16:17 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,15 @@ def key_for_min_value(ikea)
   min_key   =  nil
   ikea.each do  |key, value| if value <  min_value
                     min_value = value
+                    min_key   = key
                     puts min_value
                     puts min_key
+                  else
+                    puts "aaaaaaaaaaaaaaaaaa"
+                    puts "value"  + value
+                    puts "min_value" + min_value
+                    puts min_key
+
                  end
                end
                puts min_key
